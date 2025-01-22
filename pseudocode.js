54f63cfeab508a774debdbc45ac2cdc2caadec80
Saif Saigol <96741176+ssaigol@users.noreply.github.com>
2025-01-22 14:05:51 -0500

    Update to use classes & optimize encapsulation

--- pseudocode.js
+++ pseudocode.js
@@ -36,3 +36,157 @@
     //deleteBook function => called as handler on event listener on delete buttons
     //changeReadStatus function => called as handler on event listener on change read status buttons
             
+
+
+
+//OLD CODE - BEFORE CHANGE TO CLASSES INSTEAD OF OBJECT CONSTRUCTOR//
+
+
+//Library Array containing all books as objects
+const myLibrary = [
+    { title: "The Hobbit", author: "J.R.R. Tolkien", pages: 310, read: "Read"},
+    { title: "Wuthering Heights", author: "Emily Brontë", pages: 464, read: "Not Read"}
+];
+
+//Book object constructor
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
+    }
+}
+
+//Function that uses constructor to create new book object, and then adds it to Library array
+function addBookToLibrary (title, author, pages, read) {
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
+
+//Event listener that uses addBookToLibrary function to add new entries from form submit to library array
+const submit = document.getElementById("submit-button");
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    const newTitle = document.getElementById("title").value;
+    const newAuthor = document.getElementById("author").value;
+    const newPages = document.getElementById("pages").value;
+    const entryRead = document.getElementById("read");
+    var newRead;
+    if (entryRead.checked) {
+        newRead = "Read";
+    } else newRead = "Not Read";
+    addBookToLibrary (newTitle, newAuthor, newPages, newRead);
+    dialog.close();
+    //Repopulate table display with new book entry
+    displayBooks(myLibrary);
+    addListener();
+    dialog.style.visibility = "hidden";
+})
+
+//Event listeners for opening and closing modal form
+const dialog = document.querySelector("dialog");
+const addButton = document.querySelector("#add-book");
+const closeDialog = document.querySelector("#close");
+
+addButton.addEventListener("click", () => {
+    dialog.showModal();
+    dialog.style.visibility = "visible";
+    });
+closeDialog.addEventListener("click", () => {
+    dialog.close();
+    dialog.style.visibility = "hidden";
+});
+
+
+//Iterate through library array and display each book object as new table row in DOM
+function displayBooks (arr) {
+    //Clear display
+    const tbody = document.getElementById("body");
+    while (tbody.firstChild) {
+        tbody.removeChild(tbody.firstChild);
+    }
+    //Create new table row & cells for each object in array
+    arr.forEach((book, index) => {
+        //Create row
+        const row = document.createElement("tr");
+        //Create title table cell, populate, and append to row
+        const titleCell = document.createElement("td");
+        titleCell.textContent = book.title;
+        row.appendChild(titleCell);
+        //Create author table cell, populate, and append to row
+        const authorCell = document.createElement("td");
+        authorCell.textContent = book.author;
+        row.appendChild(authorCell);
+        //Create pages table cell, populate, and append to row
+        const pagesCell = document.createElement("td");
+        pagesCell.textContent = book.pages;
+        row.appendChild(pagesCell);
+        //Create read table cell, populate, and append to row
+        const readCell = document.createElement("td");
+        readCell.textContent = book.read;
+        row.appendChild(readCell)
+        //Create delete table cell, populate with button, and append to row
+        const optionsCell = document.createElement("td");
+        const options = document.createElement("div");
+        options.className = "options";
+        const removeButton = document.createElement("button");
+        removeButton.className = "delete";
+        //Add data attribute to correspond DOM button element with array index
+        removeButton.dataset.index = index;
+        removeButton.textContent = "Delete";
+        options.appendChild(removeButton); 
+        //Create button for changing read status if book not read
+        if (book.read === "Not Read") {
+            const changeRead = document.createElement("button");
+            changeRead.className = "change-read";
+            changeRead.dataset.index = index;
+            changeRead.textContent = "Read";
+            options.appendChild(changeRead);
+        }
+        optionsCell.appendChild(options);
+        row.appendChild(optionsCell);
+        //Append completed new row to table body
+        tbody.appendChild(row);
+    });
+}
+
+//Populate display with library array upon initial load
+displayBooks(myLibrary);
+
+//Add event listener to delete buttons, call on delete function when clicked
+function addListener () {
+    const deleteButtons = document.querySelectorAll(".delete");
+    deleteButtons.forEach( e => {
+        e.addEventListener("click", () => {
+            const deleteIndex = e.dataset.index;
+            deleteBook(deleteIndex);
+        })
+    })
+    const changeReadButtons = document.querySelectorAll(".change-read");
+    changeReadButtons.forEach( e => {
+        e.addEventListener("click", () => {
+            const changeReadIndex = e.dataset.index;
+            changeReadStatus(changeReadIndex);
+        }) 
+    })
+}
+
+//Initialize addlistener function on load
+addListener();
+
+//Delete book function that is called when delete button clicked
+function deleteBook(index) {
+    myLibrary.splice(index, 1);
+    displayBooks(myLibrary);
+    addListener();
+}
+
+
+//Function to change read status
+function changeReadStatus(index) {
+    myLibrary[index].read = "Read";
+    displayBooks(myLibrary);
+    addListener();
+}
--- script.js
+++ script.js
@@ -1,148 +1,128 @@
-//Library Array containing all books as objects
-const myLibrary = [
-    { title: "The Hobbit", author: "J.R.R. Tolkien", pages: 310, read: "Read"},
-    { title: "Wuthering Heights", author: "Emily Brontë", pages: 464, read: "Not Read"}
-];
+const myLibrary = (function () {
+    const library = [
+        { title: "The Hobbit", author: "J.R.R. Tolkien", pages: 310, readstatus: "Read"},
+        { title: "Wuthering Heights", author: "Emily Brontë", pages: 464, readstatus: "Not Read"}
+    ];
 
-//Book object constructor
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
-    }
-}
-
-//Function that uses constructor to create new book object, and then adds it to Library array
-function addBookToLibrary (title, author, pages, read) {
-    const newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
-}
-
-//Event listener that uses addBookToLibrary function to add new entries from form submit to library array
-const submit = document.getElementById("submit-button");
-submit.addEventListener('click', (e) => {
-    e.preventDefault();
-    const newTitle = document.getElementById("title").value;
-    const newAuthor = document.getElementById("author").value;
-    const newPages = document.getElementById("pages").value;
-    const entryRead = document.getElementById("read");
-    var newRead;
-    if (entryRead.checked) {
-        newRead = "Read";
-    } else newRead = "Not Read";
-    addBookToLibrary (newTitle, newAuthor, newPages, newRead);
-    dialog.close();
-    //Repopulate table display with new book entry
-    displayBooks(myLibrary);
-    addListener();
-    dialog.style.visibility = "hidden";
-})
-
-//Event listeners for opening and closing modal form
-const dialog = document.querySelector("dialog");
-const addButton = document.querySelector("#add-book");
-const closeDialog = document.querySelector("#close");
-
-addButton.addEventListener("click", () => {
-    dialog.showModal();
-    dialog.style.visibility = "visible";
-    });
-closeDialog.addEventListener("click", () => {
-    dialog.close();
-    dialog.style.visibility = "hidden";
-});
-
-
-//Iterate through library array and display each book object as new table row in DOM
-function displayBooks (arr) {
-    //Clear display
-    const tbody = document.getElementById("body");
-    while (tbody.firstChild) {
-        tbody.removeChild(tbody.firstChild);
-    }
-    //Create new table row & cells for each object in array
-    arr.forEach((book, index) => {
-        //Create row
-        const row = document.createElement("tr");
-        //Create title table cell, populate, and append to row
-        const titleCell = document.createElement("td");
-        titleCell.textContent = book.title;
-        row.appendChild(titleCell);
-        //Create author table cell, populate, and append to row
-        const authorCell = document.createElement("td");
-        authorCell.textContent = book.author;
-        row.appendChild(authorCell);
-        //Create pages table cell, populate, and append to row
-        const pagesCell = document.createElement("td");
-        pagesCell.textContent = book.pages;
-        row.appendChild(pagesCell);
-        //Create read table cell, populate, and append to row
-        const readCell = document.createElement("td");
-        readCell.textContent = book.read;
-        row.appendChild(readCell)
-        //Create delete table cell, populate with button, and append to row
-        const optionsCell = document.createElement("td");
-        const options = document.createElement("div");
-        options.className = "options";
-        const removeButton = document.createElement("button");
-        removeButton.className = "delete";
-        //Add data attribute to correspond DOM button element with array index
-        removeButton.dataset.index = index;
-        removeButton.textContent = "Delete";
-        options.appendChild(removeButton); 
-        //Create button for changing read status if book not read
-        if (book.read === "Not Read") {
-            const changeRead = document.createElement("button");
-            changeRead.className = "change-read";
-            changeRead.dataset.index = index;
-            changeRead.textContent = "Read";
-            options.appendChild(changeRead);
+    class Book {
+        constructor(title, author, pages, readstatus) {
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+            this.readstatus = readstatus;
         }
-        optionsCell.appendChild(options);
-        row.appendChild(optionsCell);
-        //Append completed new row to table body
-        tbody.appendChild(row);
+    };
+
+    const addToLibrary = (title, author, pages, readstatus) => {
+        const newBook = new Book(title, author, pages, readstatus);
+        library.push(newBook);
+        display.render();
+    };
+
+    const getLibraryArr = () => {
+        const arr = library;
+        return arr;
+    }
+
+    const deleteBookObj = (index) => {
+        library.splice(index, 1);
+    };
+
+    const changeBookObjReadStatus = (index) => {
+        library[index].readstatus = "Read";
+    };
+
+
+    return {addToLibrary, getLibraryArr, deleteBookObj, changeBookObjReadStatus};
+})();
+
+
+const display = (function () {
+    //DOM Queries
+    const addBookButton = document.getElementById("add-book");
+    const newTitle = document.getElementById("title");
+    const newAuthor = document.getElementById("author");
+    const newPages = document.getElementById("pages");
+    const newRead = document.getElementById("read");
+    const newNotRead = document.getElementById("not-read");
+    const submitBook = document.getElementById("submit-button");
+    const closeDialog = document.getElementById("close");
+    const dialog = document.getElementById("dialog");
+    const tableBody = document.getElementById("body");
+
+
+
+
+    //Display library array as table DOM element
+    const render = () => {
+        //Clear table before rendering to prevent duplicates
+        while (tableBody.firstChild) {
+            tableBody.removeChild(tableBody.firstChild);
+        };
+        myLibrary.getLibraryArr().forEach((book, index) => {
+            //Create row
+            const row = document.createElement("tr");
+            //Create title cell & populate with Book obj title property
+            const titleCell = document.createElement("td");
+            titleCell.textContent = book.title;
+            //Create author cell & populate w/ Book obj author property
+            const authorCell = document.createElement("td");
+            authorCell.textContent = book.author;
+            //Create pages cell & populate w/ Book obj pages property
+            const pagesCell = document.createElement("td");
+            pagesCell.textContent = book.pages;
+            //Create read cell & populate w/ Book obj readstatus property
+            const readCell = document.createElement("td");
+            readCell.textContent = book.readstatus;
+            //Create options cell & populate w/ div containing delete and Read buttons (for Not Read books only)
+            const optionsCell = document.createElement("td");
+            const optionsButtons = document.createElement("div");
+            optionsButtons.classList.add("options");
+            const deleteButton = document.createElement("button");
+            deleteButton.classList.add("delete");
+            deleteButton.dataset.index = index;
+            deleteButton.textContent = "Delete";
+            deleteButton.addEventListener("click", deleteBook);
+            optionsButtons.append(deleteButton);
+            if (book.readstatus === "Not Read") {
+                const readButton = document.createElement("button");
+                readButton.textContent = "Read";
+                readButton.classList.add("change-read");
+                readButton.dataset.index = index;
+                readButton.addEventListener("click", changeReadStatus);
+                optionsButtons.append(readButton);
+            }
+            optionsCell.append(optionsButtons);
+            //Append 5 new cells to row, append row to table
+            row.append(titleCell, authorCell, pagesCell, readCell, optionsCell);
+            tableBody.append(row);
+        });
+    };
+
+    const deleteBook = (e) => {
+        myLibrary.deleteBookObj(e.target.dataset.index);
+        render();
+    };
+
+    const changeReadStatus = (e) => {
+        myLibrary.changeBookObjReadStatus(e.target.dataset.index);
+        render();
+    }
+
+    //Event listeners
+    addBookButton.addEventListener("click", () => dialog.showModal());
+    closeDialog.addEventListener("click", () => dialog.close());
+    submitBook.addEventListener("click", () => {
+        const title = newTitle.value;
+        const author = newAuthor.value;
+        const pages = newPages.value;
+        let read;
+        if (newRead.checked) {
+            read = "Read";
+        } else read = "Not Read";
+        myLibrary.addToLibrary(title, author, pages, read);
     });
-}
+    window.addEventListener("load", render);
 
-//Populate display with library array upon initial load
-displayBooks(myLibrary);
-
-//Add event listener to delete buttons, call on delete function when clicked
-function addListener () {
-    const deleteButtons = document.querySelectorAll(".delete");
-    deleteButtons.forEach( e => {
-        e.addEventListener("click", () => {
-            const deleteIndex = e.dataset.index;
-            deleteBook(deleteIndex);
-        })
-    })
-    const changeReadButtons = document.querySelectorAll(".change-read");
-    changeReadButtons.forEach( e => {
-        e.addEventListener("click", () => {
-            const changeReadIndex = e.dataset.index;
-            changeReadStatus(changeReadIndex);
-        }) 
-    })
-}
-
-//Initialize addlistener function on load
-addListener();
-
-//Delete book function that is called when delete button clicked
-function deleteBook(index) {
-    myLibrary.splice(index, 1);
-    displayBooks(myLibrary);
-    addListener();
-}
-
-
-//Function to change read status
-function changeReadStatus(index) {
-    myLibrary[index].read = "Read";
-    displayBooks(myLibrary);
-    addListener();
-}
+    return {render};
+})();
